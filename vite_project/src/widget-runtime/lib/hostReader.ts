14410373603175e4ed8e3e0088a648ed20e84eb7
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-05-27 18:51:09 +0100

    Simplified the hosting part to leave Caddy handling hosting as a strong fit

--- vite_project/src/widget-runtime/lib/hostReader.ts
+++
@@ -1,7 +0,0 @@
-export function getMountedHost(hostElement: HTMLElement) {
-    return hostElement;
-    const shadow =
-        hostElement.shadowRoot || hostElement.attachShadow({ mode: "open" });
-
-    return shadow
-}
